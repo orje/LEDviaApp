811b9c917549e1d4c4bcf3806d9f3e3c258f3b92
Orje <j.brulow@kabelmail.de>
2017-12-13 20:21:23 +0100

    first thoughts

---
+++ doc/stuff/MoreRunningLED.mm
@@ -0,0 +1,38 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1513188003606" ID="ID_435379548" MODIFIED="1513191847737" TEXT="MoreRunningLED">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Ziel:
+    </p>
+    <p>
+      &#220;ber einen + oder - Button die Anzahl der &quot;rennenden&quot; LED erh&#246;hen und vermindern
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1513188202084" ID="ID_857246757" MODIFIED="1513188221324" POSITION="right" TEXT="App erg&#xe4;nzen">
+<node CREATED="1513188589208" ID="ID_768610870" MODIFIED="1513189221021" TEXT="Buttons"/>
+<node CREATED="1513190919608" ID="ID_438396013" MODIFIED="1513190927384" TEXT="Variablen"/>
+</node>
+<node CREATED="1513188222950" ID="ID_1963567979" MODIFIED="1513188230511" POSITION="right" TEXT="Model erg&#xe4;nzen">
+<node CREATED="1513191125962" ID="ID_317245904" MODIFIED="1513191964006" TEXT="Algorithmus &#xfc;berlegen">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      im Model rechnen
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</map>
